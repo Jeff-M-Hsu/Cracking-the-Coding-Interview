4f872cc88ecbf760afed5570589317b5a0ccf501
jhsu42 <jhsu42@uwo.ca>
2018-10-13 22:03:59 -0700

    Added a method that uses a data structure
    Added a method that uses bit vectors for personal education, runtime is
    O(n)

--- IsUnique/src/IsUnique.java
+++ IsUnique/src/IsUnique.java
@@ -1,23 +1,42 @@
+/*
+ * Implement an algorithm to determine if a string has all unique characters. 
+ * What if you cannot use additional data structures?
+ */
 
 public class IsUnique {
 	public static void main(String args[]) {
-		if(isUnique(args[0]))
-			System.out.println("Unique");
-		else
-			System.out.println("Not Unique");
+		String[] tests = {"abcd", "abccd", "apple", "orange"};
+		for(String words : tests) {
+			if(isUnique(words))
+				System.out.println("Unique");
+			else
+				System.out.println("Not Unique");
+		}
 	}
-//abcde
-//aabcd
+	
+	//O(n^2)
 	public static boolean isUnique(String input) {
 		for(int i = 0; i < input.length()-1; i++) {
 			char c = input.charAt(i);
 			for(int j = i+1; j < input.length(); j++) {
-				if(input.charAt(j) == c) {
+				if(c == input.charAt(j))
 					return false;
-				}
 			}
 		}
 		return true;
 	}
-}
 
+	//O(n) but uses a data structure
+	public static boolean isUniqueDS(String input) {
+		if(input.length() > 26) return false; //26 letters max
+		int bit = 0;
+		for(int i = 0; i < input.length(); i++) {
+			int bitVal = input.charAt(i)-'a'; //a-a = 0, z-a = 26
+			if((bit & (1 << bitVal)) > 0) //check if there is a 1 in bit at the digit of bitVal
+				return false; //if there is then we've already seen this letter
+			bit |= (1 << bitVal); //if there isn't then we use |= (OR operator) to add the 1 to bit at that index
+		}
+		return true;
+	}
+}
+	
