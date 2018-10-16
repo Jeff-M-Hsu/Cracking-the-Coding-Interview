a852b64d304812ffd53c62ae875a315c6017c9c5
jhsu42 <jhsu42@uwo.ca>
2018-10-15 22:39:38 -0700

    Previous brute force solution actually O(n^2) due to string concatenation
    This solution is O(n) in cases where the compressed string is shorter
    than the original. Worst case would still be O(n^2). However, we can
    re-purpose the code that performs the compression to count the final
    length of the resultant string and create a StringBuilder of that size
    to maintain a O(n) runtime for all cases in exchange for ~2x the amount
    of code.

--- StringCompression/src/StringCompression.java
+++ StringCompression/src/StringCompression.java
@@ -6,29 +6,34 @@
  */
 public class StringCompression {
 	public static void main(String[] args){
-		String[] words = {"aaaaa", "aaabbbcccaaa", "abcabcabcabc"};
+		String[] words = {"aaabbbcccabb", "aaabbbcccaaa", "aaaaa", "abcabcabcabc"};
 		for(String w : words){
 			System.out.println(compress(w));
 		}
 	}
 	
-	//O(n) brute force solution
+	//This solution is O(n) in cases where the compressed string is shorter than the original. 
+	//Worst case would still be O(n^2). However, we can re-purpose the code that performs the 
+	//compression to count the final length of the resultant string and create a StringBuilder 
+	//of that size to maintain a O(n) runtime for all cases in exchange for ~2x the amount of code.
 	public static String compress(String word){
-		String result = "";
+		StringBuilder result = new StringBuilder(word.length());
 		char c = word.charAt(0);
 		int count = 1;
 		for(int i = 1; i < word.length(); i++){
 			if(word.charAt(i) == c)
 				count++;
 			else{
-				result += "" + c + "" + count;
+				result.append(c);
+				result.append(count);
 				c = word.charAt(i);
 				count = 1;
 			}
 		}
-		result += "" + c + "" + count;
+		result.append(c);
+		result.append(count);
 		if(result.length() > word.length())
 			return word;
-		else return result;
+		else return result.toString();
 	}
 }
